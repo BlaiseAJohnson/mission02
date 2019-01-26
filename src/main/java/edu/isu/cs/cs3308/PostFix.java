64c488926c405f4620f289e75f3e6c4e45c59829
Blaise Johnson <blaiseajohnson@gmail.com>
2019-01-26 16:06:10 -0700

    Fully implement PostFix.
    All tests now pass.

--- src/main/java/edu/isu/cs/cs3308/PostFix.java
+++ src/main/java/edu/isu/cs/cs3308/PostFix.java
@@ -1,5 +1,7 @@
 package edu.isu.cs.cs3308;
 
+import edu.isu.cs.cs3308.structures.impl.LinkedStack;
+
 /**
  * Postfix expression evaluator.
  *
@@ -17,7 +19,72 @@ public class PostFix {
      * @return value of the postfix express or 0 if the postfix expression is null,
      * empty, or contains only whitespace.
      */
-    public static int evalPostFix(String infix) {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public static int evalPostFix(String postfix) {
+        if (postfix == null || postfix.replaceAll(" ", "").equals("")) return 0;
+
+        LinkedStack<Integer> stack = new LinkedStack<>();
+
+        String[] equation = postfix.split(" ");
+
+        for (String currentSymbol : equation) {
+
+            if (!currentSymbol.equals(" ")) {
+                if (intTryParse(currentSymbol)) {
+                    int currentNum = Integer.parseInt(currentSymbol);
+                    stack.push(currentNum);
+                }
+                else performOperation(currentSymbol, stack);
+            }
+        }
+
+        return stack.pop();
+    }
+
+    /**
+     * Determines whether a string can be parsed into an integer.
+     * @param intChar The string for which to determine the ability to be parsed.
+     * @return A boolean representing whether the string is able to be parsed.
+     */
+    private static boolean intTryParse(String intChar) {
+        try {
+            Integer.parseInt(intChar);
+
+            return true;
+        }
+        catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    /**
+     * Performs the specified operation on the top two elements of the stack,
+     * then pushes the result back onto the stack.
+     * @param operator The operator to perform as a string.
+     * @param stack The stack upon which to perform the operation.
+     */
+    private static void performOperation(String operator, LinkedStack<Integer> stack) {
+        if (stack.size() < 2) throw new IllegalArgumentException();
+
+        int num2 = stack.pop();
+        int num1 = stack.pop();
+
+        switch (operator) {
+            case "+":
+                stack.push(num1 + num2);
+                break;
+            case "-":
+                stack.push(num1 - num2);
+                break;
+            case "*":
+                stack.push(num1 * num2);
+                break;
+            case "/":
+                stack.push(num1 / num2);
+                break;
+            default:
+                stack.push(num1);
+                stack.push(num2);
+                break;
+        }
     }
 }
